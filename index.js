be79a76f54c097e2eae95ccad1ea5c41f9913929
ko-math <sorehakiemasu@gmail.com>
2026-09-18 09:55:12 +0900

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const data = [
   ['n(10)','n(2)','max']
 ];
 const N = 7;
-const MIN = 120;//2 ** (N - 1);
+const MIN = 120; //2 ** (N - 1)
 const MAX = 2 ** N - 1;
 for(let i = MIN;i <= MAX;i++){
   data.push([i,i.toString(2),max(i)]);
